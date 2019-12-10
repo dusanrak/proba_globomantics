e5d768d581d9d10f24bf11ebdd4d3c910ec0eb4c
dusanrak <dusanrak@telekom.rs>
2019-12-10 13:40:25 +0100

    deleted USD added $

--- database.py
+++ database.py
@@ -49,7 +49,7 @@ class Database:
             bal = float(acct["due"]) - float(acct["paid"])
 
             # Style added in module 4
-            return f"{bal:.2f} "
+            return f"$ {bal:.2f} "
 
             # Style added in module 3
             # return f"$ {bal:.2f}"
